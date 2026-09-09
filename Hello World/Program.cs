2f6a56e816846a914a2049c7d90c6d939c761e65
Bryce Clement <bryceclement6@gmail.com>
2026-09-09 14:35:44 -0400

    Add header comment and introduction lines

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+/*
+* Name: Bryce Clement
+* Course: CSCI 1250, Section 002
+* Assignment: Lab 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Bryce Clement.");
+Console.WriteLine("This is my first program at ETSU.");
